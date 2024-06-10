9df67513f37aff92e6c8c13f86c1d165fe8e3624
BryanEstrada003 <mbestrad@espol.edu.ec>
2024-06-10 10:09:49 -0500

    Modify structure in fetch

--- js/main.js
+++ js/main.js
@@ -4,4 +4,50 @@ let loaded = (eventLoaded) => {
     myform.addEventListener("submit", (eventSubmit) => {
         debugger;
     });
+
+    constformulario = document.getElementById("formulario");
+    constformulario.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        nombre = document.getElementById("nombre");
+        email = document.getElementById("email");
+
+        if (nombre.value.length == 0) {
+            alert("Nombre requerido")
+            nombre.focus()
+            return;
+        }
+
+        if (email.value.length == 0) {
+            alert("Email requerido")
+            email.focus()
+            return;
+        }
+
+        constnombre = document.getElementById("nombre").value;
+        constemail = document.getElementById("email").value;
+
+        constdatos = {
+            nombre: constnombre,
+            email: constemail,
+        };
+
+        fetch(
+            "https://lavender-59c67-default-rtdb.firebaseio.com/lavender.json",
+            {
+                method: "POST",
+                body: JSON.stringify(constdatos),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+            .then((respuesta) => respuesta.json())
+            .then((datos) => {
+                console.log(datos);
+            })
+            .catch((error) => console.error(error));
+    });
 };
+
+window.addEventListener("DOMContentLoaded", loaded);
